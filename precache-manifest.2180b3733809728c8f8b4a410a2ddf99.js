60015326b4d1ff6cdba641fe316cc3892f200824
unknown <chubais@sveta.net>
2020-01-13 02:39:56 +0300

    Updates

---
+++ precache-manifest.2180b3733809728c8f8b4a410a2ddf99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678f3c9d58a32b877ad00afb0dcb0a30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0302240de2dc82916bff",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "cf0f16f08ee9a928f549",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0302240de2dc82916bff",
+    "url": "/static/js/2.9cc11781.chunk.js"
+  },
+  {
+    "revision": "73d69586ba839c1fde7075eee69442a0",
+    "url": "/static/js/2.9cc11781.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf0f16f08ee9a928f549",
+    "url": "/static/js/main.7699d2f3.chunk.js"
+  },
+  {
+    "revision": "25ba10bb4dbf5b12a0df",
+    "url": "/static/js/runtime-main.f1cbbf49.js"
+  }
+]);
